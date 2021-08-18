bcdddf508ad9a9c5b5015635aaa3b81881038ed8
EmanAmassi <eman.amassi98@gmail.com>
2021-08-18 15:05:25 +0300

    insert into db tables

--- server/database/config/init.sql
+++ server/database/config/init.sql
@@ -26,4 +26,24 @@ CREATE TABLE user_follow(
     following_id INTEGER NOT NULL
 );
 
+
+INSERT INTO users(user_name, name) VALUES 
+('nourzada', 'Nour Adawi'),
+('EmanWamda', 'Eman Amassi'),
+('moon', 'AbeerAydi');
+
+
+INSERT INTO user_follow (follower_id, following_id) VALUES
+(1,2),
+(1,3),
+(2,1),
+(2,3),
+(3,1),
+(3,2);
+
+
+Insert Into posts (user_id, text_content) VALUES
+(2,'هذا العالم عجيب إلى أبعد حد #بقلمي'),
+(1, 'ثمة مساحة بين الحافز والرد، في تلك المساحة تكمن قوتنا في اختيار ردنا، وفي ردنا يكمن نمونا وحريتنا')
+(3, 'البدر هذه الليلة يشبهني')
 COMMIT;
